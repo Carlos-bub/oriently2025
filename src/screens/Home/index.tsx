91088a8af29a74adadf3554d3e26570f725b0de6
Carlos-bub <chln@aluno.ifnmg.edu.br>
2025-02-19 11:01:58 -0300

    Header_tela_home_e_splash

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,42 @@
-import { Text } from 'react-native';
-import { Container, Title} from './styles';
+
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  UserIcon,
+  HeaderTextWrapper,
+  Greenting,
+  Hello,
+  LogoutButton,
+  LogoutIcon
+} from './styles';
 
 export function Home() {
   return (
     <Container>
-      <Title>Oriently 2025</Title>
+      <Header> 
+        <UserWrapper>
+          <UserInfo>
+            <UserIcon name = 'user-circle' >
+              
+            </UserIcon>
+            <HeaderTextWrapper>
+              <Greenting>
+                Olá
+              </Greenting>
+              <Hello>
+                Usuário
+              </Hello>
+            </HeaderTextWrapper>
+          </UserInfo>
+          <LogoutButton>
+            <LogoutIcon name = 'logout'>
+
+            </LogoutIcon>
+          </LogoutButton>
+        </UserWrapper>
+      </Header> 
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,15 +2,65 @@
 import styled from 'styled-components/native';
 import { DefaultTheme } from 'styled-components/native';
 import theme from '../../global/styles/theme';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+
+import { MaterialIcons  } from '@expo/vector-icons';
+import { FontAwesome  } from '@expo/vector-icons';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${(props: { theme: DefaultTheme }) => props.theme.colors.background};
-  align-items: center;
-  justify-content: center;
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
-  font-family: ${theme.fonts.Titulos};
-`;
+  export const Header = styled.View`
+      width: 100%;
+      height: ${RFPercentage(20)}px;
+      background-color: ${(props: { theme: DefaultTheme }) => props.theme.colors.primary};
+      //align-items: center;
+      //justify-content: center;
+    `;
+
+  export const UserWrapper = styled.View`
+    flex-direction: row;
+    padding: 0 25px;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: ${getStatusBarHeight() + RFValue(45)}px;
+  `;
+
+  export const UserInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+  `;
+
+  export const UserIcon = styled(FontAwesome )`
+    color: ${(props: { theme: DefaultTheme }) => props.theme.colors.background};
+    font-size: ${ RFValue(40)}px;
+    
+  `;
+
+  export const HeaderTextWrapper = styled.View`
+    margin-left: 15;
+  `;
+
+  export const Greenting = styled.Text`
+    color: ${(props: { theme: DefaultTheme }) => props.theme.colors.background};
+    font-family: ${theme.fonts.buttons};
+    font-size: ${ RFValue(16)}px;
+    line-height: ${ RFValue(24)}px;
+  `;
+
+  export const Hello = styled.Text`
+    color: ${(props: { theme: DefaultTheme }) => props.theme.colors.background};
+    font-family: ${theme.fonts.Subtitulos};
+    font-size: ${ RFValue(20)}px;
+    line-height: ${ RFValue(24)}px;
+  `;
+
+  export const LogoutButton = styled.View``;
+
+  export const LogoutIcon = styled(MaterialIcons)`
+    color: ${(props: { theme: DefaultTheme }) => props.theme.colors.background};
+    font-size: ${ RFValue(30)}px;
+  `;
